ed26cb82d19a270781b8323b2d6906ed61d15c1d
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2024-08-06 00:40:38 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -59,7 +59,7 @@ const UpdateToDo = (text) => {
             ToDoTitle.innerText = text;
         }
     })
-};
+}
 
 const searchToDos = (term) => {
     const ToDos = document.querySelectorAll(".ToDo");
